e1b752da7dcff2a80ca25e8a760a6406ce764a03
Erin Woodworth <elwoodworth@gmail.com>
2016-12-08 15:27:08 -0800

    working to get dateutil in the right place

--- helpers.py
+++ helpers.py
@@ -1,8 +1,8 @@
 from flask import session
 from model import User, Address, Chore, Userchore
 
-from dateutil.relativedelta import *
-from dateutil.rrule import *
+from python-dateutil.relativedelta import *
+from python-dateutil.rrule import *
 from datetime import datetime
 
 MY_COLOR_FAMILY = ['#CE93D8', '#B39DDB', '#9FABDA', '#90CAF9', '#81d4fa', '#8ddeea', 
