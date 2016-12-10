42ae729954d6a857dc6f1fa090727e9162ee1523
Erin Woodworth <elwoodworth@gmail.com>
2016-12-10 14:23:40 -0800

    working to get dateutil in the right place

--- server.py
+++ server.py
@@ -295,7 +295,7 @@ if __name__ == "__main__":
     # that we invoke the DebugToolbarExtension
 
     # Do not debug for demo
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
     # Use the DebugToolbar
