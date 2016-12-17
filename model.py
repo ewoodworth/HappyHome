449fed96b8c9c888c7b4a3afc5fd934601fd254d
Erin Woodworth <elwoodworth@gmail.com>
2016-12-16 19:56:26 -0800

    Fixing first pass routing sqla markup

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 """Models and database functions for final project."""
 
 
-from flask-sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # import correlation
 
 # This is the connection to the PostgreSQL database; we're getting this through
